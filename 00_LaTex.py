64d4de244205d73017659225fa55291b931a39d2
André Luis Marques Ferreira dos Santos <andrelmfsantos@gmail.com>
2020-04-12 13:30:57 -0300

    Add files via upload

---
+++ 00_LaTex.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # LaTex
+
+# * [Markdown Cells](https://jupyter-notebook.readthedocs.io/en/stable/examples/Notebook/Working%20With%20Markdown%20Cells.html)
+# 
+# * [Mathematical Python](https://www.math.ubc.ca/~pwalls/math-python/jupyter/markdown/)
+# 
+# * [Writing Mathematical Expressions with Latex](https://link.springer.com/content/pdf/bbm%3A978-1-4842-3913-1%2F1.pdf)
+# 
+
+# ### Escrevendo expressões com Markdown e LaTex:
+
+# In[17]:
+
+
+# Exemplos
+
+# Ênfase:
+#*itálico*
+#**negrito**
+#`<código>`
+#-----------
+
+# Listas
+#1. Jupyter
+#    * Notebook
+#    * Markdown
+#    * Latex
+#2. Python
+#    * Datatypes and Operators
+#    * Functions
+#    * Logic and Loops
+#    * Modules and Packages
+#3. Scientific Computating in Python
+#    * NumPy
+#    * Matplotlib
+#    * SciPy
+#    * pandas
+# ----------------------------------
+
+# Link
+#[UBC Math](http://www.math.ubc.ca)
+# ---------------------------------------
+
+# Imagem:
+#![Uninove logo](http://www.sinergia.edu.br/wp-content/uploads/2016/10/uninove-logo-1200x480.jpg)
+# -----------------------------------------------------------------------------------------------
+
+# Tabela:
+#| Python Operator | Description  |
+#| :---: | :---: |
+#| `+` | addition |
+#| `-` | subtraction |
+#| `*` | multiplication |
+#| `/` | division |
+#| `**` | power |
+# ----------------------------------
+
+# Equações:
+# $$e^x=\sum_{i=0}^\infty \frac{1}{i!}x^i$$
+# -----------------------------------------
+
+
+# *itálico*
+# **negrito**
+# `<código>`
+
+# 1. Jupyter
+#     * Notebook
+#     * Markdown
+#     * Latex
+# 2. Python
+#     * Datatypes and Operators
+#     * Functions
+#     * Logic and Loops
+#     * Modules and Packages
+# 3. Scientific Computating in Python
+#     * NumPy
+#     * Matplotlib
+#     * SciPy
+#     * pandas
+
+# [UBC Math](http://www.math.ubc.ca)
+
+# ![Uninove logo](http://www.sinergia.edu.br/wp-content/uploads/2016/10/uninove-logo-1200x480.jpg)
+
+# | Python Operator | Description  |
+# | :---: | :---: |
+# | `+` | addition |
+# | `-` | subtraction |
+# | `*` | multiplication |
+# | `/` | division |
+# | `**` | power |
+
+# $$e^x=\sum_{i=0}^\infty \frac{1}{i!}x^i$$

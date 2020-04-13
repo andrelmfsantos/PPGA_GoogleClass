b3432cc97f6e8a77f88203989d5b4cd2b8f99476
André Luis Marques Ferreira dos Santos <andrelmfsantos@gmail.com>
2020-04-13 16:44:34 -0300

    Add files via upload

---
+++ 01_Matematica_Basica.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <img src="http://www.sinergia.edu.br/wp-content/uploads/2016/10/uninove-logo-1200x480.jpg" align="middle" style="width:120px;height:48px"/>
+
+# ## Operações elementares da matemática
+# [Overleaf: Site com Expressões Matemáticas em LaTex](https://www.overleaf.com/learn/latex/List_of_Greek_letters_and_math_symbols)
+
+# | Python Operator | Description  |
+# | :---: | :---: |
+# | `+` | addition |
+# | `-` | subtraction |
+# | `*` | multiplication |
+# | `/` | division |
+# | `//` | division 2|
+# | `**` | power |
+# | `%`| module |
+
+# | Python Operator | Description  | Exemplos |
+# | :---: | :---: | :---: |
+# | `+` | addition | $$5+3$$ |
+# | `-` | subtraction | $$5-3$$ |
+# | `*` | multiplication | $$5\times3$$ |
+# | `/` | division | $$5\div3$$ |
+# | `**` | power | $$5^3$$ |
+
+# In[15]:
+
+
+5+3 # soma
+
+
+# In[2]:
+
+
+5-3 # subtração
+
+
+# In[3]:
+
+
+5 * 3 # multiplicação
+
+
+# In[17]:
+
+
+5/3 # divisão: a parte inteira e decimal na resposta
+
+
+# In[16]:
+
+
+5//3 # divisão: a parte inteira na resposta
+
+
+# In[6]:
+
+
+5%3 # divisão (módulo ou resto)
+
+
+# In[7]:
+
+
+5**3 # potenciação
+
+
+# In[8]:
+
+
+# Operações elementares da matemática
+5+3 # soma
+5-3 # subtração
+5*3 # multiplicação
+5/3 # divisão: a parte inteira e decimal na resposta
+
+
+# In[14]:
+
+
+print('soma:         ',5+3)
+print('subtração:    ',5-3)
+print('multiplicação:',5*3)
+print('divisão:      ',5/3)
+
